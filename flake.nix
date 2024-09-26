a285ac77e65a155c35a41223ca5cc7fd678452fd
heinwol <heinvvol@gmail.com>
2024-09-26 15:39:57 +0300

    dev: set up environment, dependencies && migrated

--- flake.nix
+++ flake.nix
@@ -11,42 +11,38 @@
     };
   };
 
-  outputs = inputs@{ flake-parts, devenv, poetry2nix, ... }:
+  outputs = inputs@{ flake-parts, devenv, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         devenv.flakeModule
       ];
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+        let
+          poetry2nix = import inputs.poetry2nix { inherit pkgs; };
+        in
+        {
 
-        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-        # packages.default = pkgs.hello;
-        devenv.shells = {
-          default = {
-            packages = [ pkgs.git ];
-            languages.python = {
-              enable = true;
-              package = pkgs.python312;
-              poetry = {
+          devenv.shells = {
+            default = {
+              # packages = [ pkgs.poetry ];
+              languages.python = {
                 enable = true;
-                activate.enable = true;
-                install.enable = true;
+                package = pkgs.python312;
+                poetry = {
+                  enable = true;
+                  activate.enable = true;
+                  install.enable = true;
+                };
               };
             };
-            enterShell = ''
-              hello
-            '';
           };
+          # myPythonApp = poetry2nix.mkPoetryApplication { projectDir = ./.; };
         };
-      };
       flake = {
         # The usual flake attributes can be defined here, including system-
         # agnostic ones like nixosModule and system-enumerating ones, although
         # those are more easily expressed in perSystem.
-        myPythonApp = poetry2nix.mkPoetryApplication { projectDir = ./.; };
       };
     };
 }
